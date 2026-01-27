5f356ded5c14e1bebd624b5b5beb69a1f11b6227
emelyanov2277 <emelyanov2277@gmail.com>
2026-01-27 15:42:44 +0500

    Первый коммит

---
+++ home_work_1/first_script.py
@@ -0,0 +1,71 @@
+# name = 'Alex'
+# surname = 'Emel'
+# worksss = 5
+# workers = 'Quality Assurance'
+# print(f'Мое полное имя {name} {surname} и я проработал в тензоре {worksss} на должности {workers}')
+
+# city = 'pskov'
+# ticket_price=120
+# seat='33b'
+# date='12.12.2015'
+# print(f'Билет на поезд/самолёт\n'
+#       f'Город: Санкт-Петербург → {city}\n'
+#       f'Дата: {date}\n'
+#       f'Место: {seat}\n'
+#       f'Цена: {ticket_price}')
+
+# product='Наушники Sony WH-1000XM5'
+# price=34990
+# quantity=2
+# discount=15
+# formula=(price*quantity)-(((price*quantity)*discount)/100)
+# print(f'Чек\n'
+#       f'Товар: {product}\n'
+#       f'Цена: {price}\n'
+#       f'Количество: {quantity}\n'
+#       f'Скидка: {discount}\n'
+#       f'──────────────────────\n'
+#       f'Итого к оплате: {formula}\n')
+
+# first_name=input('Введи свое имя = ')
+# last_name=input('Введи свою фамилию = ')
+# age=int(input('Введи свой возвраст = '))
+# print(f'Привет, {first_name} {last_name}!\n'
+#       f'Тебе уже {age} лет?\n'
+#       f'Круто, что ты увлекаешься программированием!')
+
+# name=input('Введи свое имя')
+# birth_year=int(input('Введи свой год'))
+# height_cm=float(input('Введи свой рост'))
+# weight_kg=float(input('Введи свой вес'))
+# voz=2025-birth_year
+# height_m = height_cm / 100
+# bmi = weight_kg / (height_m ** 2)
+# print(f'Анкета\n'
+#       f'Имя = {name}\n'
+#       f'Год рождения = {birth_year}\n'
+#       f'Возвраст = {voz}\n'
+#       f'Рост = {height_cm}\n'
+#       f'Вес = {weight_kg}\n'
+#       f'ИМТ = {bmi}\n')
+# if bmi <25 and bmi >19 :
+#      print('Четкий ИМТ')
+
+# name=input('Введи свое имя')
+# birth_year=int(input('Введи свой год'))
+# height_cm=float(input('Введи свой рост'))
+# weight_kg=float(input('Введи свой вес'))
+# voz=2025-birth_year
+# height_m = height_cm / 100
+# bmi = weight_kg / (height_m ** 2)
+# print(f'Анкета\n'
+#       f'Имя = {name}\n'
+#       f'Год рождения = {birth_year}\n'
+#       f'Возвраст = {voz}\n'
+#       f'Рост = {height_cm}\n'
+#       f'Вес = {weight_kg}\n'
+#       f'ИМТ = {bmi}\n')
+# if bmi <25 and bmi >19 :
+#      print('Четкий ИМТ')
+
+
